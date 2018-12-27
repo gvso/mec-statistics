242db56ef3e20cde18b58cde1a4ada65750d7b35
Getulio Sánchez <valentin2507@gmail.com>
2018-12-26 20:27:11 -0500

    Estimate retiring teachers

--- scripts/docentes.R
+++ scripts/docentes.R
@@ -3,6 +3,7 @@ library(stringr)
 DIRECTORY <- "/home/gvso/Data/MEC/data/docentes/"
 CUR_YEAR <- 2018
 STARTING_YEAR <- 1985
+RETIRING_YEAR <- 25
 
 file <- paste0(DIRECTORY, "funcionarios_docentes_2018_9.csv")
 
@@ -28,24 +29,6 @@ data2018$seniority_month <- sapply(data2018$antiguedad_docente, function(value) 
 data2018$antiguedad_docente <- NULL
 
 #########################################################
-###### Estimates number of new teachers per year ########
-#########################################################
-plot_delta_teachers <- function() {
-  year <- sapply(data2018$seniority_year, function(seniority_year) {
-    CUR_YEAR - seniority_year
-  })
-  
-  hirings <- as.data.frame(table(year))
-  
-  years <- as.numeric(levels(hirings$year))[hirings$year]
-  freq <- hirings$Freq
-  
-  plot(years, freq, type='l', xaxt="n", xlab='Año', ylab='Nuevos docentes', 
-       xlim=c(STARTING_YEAR, CUR_YEAR), col='purple', lty=3, lwd=2)
-  axis(1, at = seq(STARTING_YEAR, CUR_YEAR, by = 2), las=2) 
-}
-
-#########################################################
 ######## Estimates number of teachers per year ##########
 #########################################################
 plot_number_of_teachers <- function() {
@@ -65,5 +48,46 @@ plot_number_of_teachers <- function() {
   plot(CUR_YEAR - seniority_years, number_teachers, xaxt="n", xlab='Año', ylab='Cantidad de docentes (aproximado)', 
        type='l', col='purple', lty=3, lwd=2)
   axis(1, at = seq(STARTING_YEAR, CUR_YEAR, by = 2), las=2)
+}
+
+#########################################################
+###### Estimates number of new teachers per year ########
+#########################################################
+plot_delta_teachers <- function() {
+  year <- sapply(data2018$seniority_year, function(seniority_year) {
+    CUR_YEAR - seniority_year
+  })
+  
+  hirings <- as.data.frame(table(year))
+  
+  years <- as.numeric(levels(hirings$year))[hirings$year]
+  freq <- hirings$Freq
+  
+  hirings_table <- data.frame(year=years, teachers=freq)
+  
+  print(hirings_table)
+  
+  plot(years, freq, type='l', xaxt="n", xlab='Año', ylab='Nuevos docentes', 
+       xlim=c(STARTING_YEAR, CUR_YEAR), col='purple', lty=3, lwd=2)
+  axis(1, at = seq(STARTING_YEAR, CUR_YEAR, by = 2), las=2) 
+}
+
+#########################################################
+#### Estimates number of retiring teachers per year #####
+#########################################################
+plot_retiring_teachers <- function() {
+  years <- 2019:2030
+  
+  retiring <- sapply(years, function(year) {
+    nrow(data2018[CUR_YEAR + (RETIRING_YEAR - data2018$seniority_year) == year, ])
+  })
+  
+  retiring_table <- data.frame(year=years, teachers=retiring)
+  
+  print(retiring_table)
+  
+  plot(years, retiring, xaxt="n", xlab='Año', ylab='Cantidad de docentes (aproximado)', 
+       type='l', col='purple', lty=3, lwd=2)
+  axis(1, at = seq(CUR_YEAR, 2030, by = 2), las=2)
 }
 
